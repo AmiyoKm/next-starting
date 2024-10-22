5c5db26f40c7fe3d49eb1e45390e61582370008c
AmiyoKm <omimondol18@gmail.com>
2024-10-22 23:09:07 +0600

    redirections

--- app/userlist/[id]/page.tsx
+++ app/userlist/[id]/page.tsx
@@ -1,10 +1,15 @@
 "use client"
+
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const User = ({params}: any) => {
+  if(params.id === '1'){
+    redirect('/login')
+  }
     console.log(params)
   return (
-    <h1>Hi this is {params.users}</h1>
+    <h1>Hi this is {params.id}</h1>
   )
 }
 
